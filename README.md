31333c36108cabdc6d329f6e4bdc76af40016024
LOU Xun <aquarhead@ela.build>
2020-05-12 09:40:39 +0000

    Revert to native-tls and works on OS X at least

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 RabbitMQ Connection Tester, pronounced as "rekt".
 
-Start with `RUST_LOG=rct ./rct`, or in PowerShell: `$ENV:RUST_LOG = "rct"`. Then toggle logs from internal crates if necessary following [the `env_logger` documentation](https://docs.rs/env_logger/0.7.1/env_logger/).
+- Requires native TLS support (`openssl` etc..)
+- Start with `RUST_LOG=rct ./rct`, or in PowerShell: `$ENV:RUST_LOG = "rct"`
+  - This only prints logs from `rct` itself, toggle logs from internal crates if necessary following [the `env_logger` documentation](https://docs.rs/env_logger/0.7.1/env_logger/).
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
 use anyhow::Result;
-use lapin::{options::*, types::FieldTable, BasicProperties, Connection, ConnectionProperties, ExchangeKind};
-use log::{debug, info};
-use std::time::Duration;
+use lapin::{Connection, ConnectionProperties};
+use log::info;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "example", about = "RabbitMQ Connection Tester")]
 struct Opt {
-  #[structopt(short, long, requires_all(&["username", "password", "port", "vhost", "encrypted"]))]
+  #[structopt(short, long, requires_all(&["username", "password", "port", "vhost"]))]
   host: Option<String>,
-  #[structopt(short, long, requires_all(&["host", "password", "port", "vhost", "encrypted"]))]
+  #[structopt(short, long, requires_all(&["host", "password", "port", "vhost"]))]
   username: Option<String>,
-  #[structopt(short, long, requires_all(&["host", "username", "port", "vhost", "encrypted"]))]
+  #[structopt(short, long, requires_all(&["host", "username", "port", "vhost"]))]
   password: Option<String>,
-  #[structopt(short = "P", long, requires_all(&["host", "username", "password", "vhost", "encrypted"]))]
+  #[structopt(short = "P", long, requires_all(&["host", "username", "password", "vhost"]))]
   port: Option<String>,
-  #[structopt(short, long, requires_all(&["host", "username", "password", "port", "encrypted"]))]
+  #[structopt(short, long, requires_all(&["host", "username", "password", "port"]))]
   vhost: Option<String>,
   #[structopt(short, long, help = "If enabled, uses `amqps` rather than `amqp` protocol", requires_all(&["host", "username", "password", "port", "vhost"]))]
   encrypted: bool,
   #[structopt(
     long,
     help = "Specify the full AMQP connection string, overrides other options",
-    required_unless_all(&["host", "username", "password", "port", "vhost", "encrypted"])
+    required_unless_all(&["host", "username", "password", "port", "vhost"])
   )]
   uri: Option<String>,
   // #[structopt(short, long, help = "Test if an exchange exists (using passive exchange.declare)")]
